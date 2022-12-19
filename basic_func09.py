d810798ffbd8725876a32c376816eb3f42c9e1d1
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2022-12-19 23:31:40 +0500

    cod09

--- basic_func09.py
+++ basic_func09.py
@@ -1,5 +1,8 @@
 # Create a function called main.
-
+def main(a):
 # Create function arguments a.
-
+    s=a-1
 # decrease the value of a to one and return
+    return s
+javob=main(12)
+print(javob)
